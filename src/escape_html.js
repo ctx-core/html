88da2cb0c672a80f5414c8ddbb638f9ec90d00ea
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:11 -0400

    ## 8.3.0
    ### Minor Changes
    
    - fix tsc build directory issues
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/object@18.2.0

--- src/escape_html.js
+++
@@ -1,9 +0,0 @@
-export function escape_html(unsafe_html) {
-    return unsafe_html
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#039;');
-}
-export { escape_html as escape__html };
