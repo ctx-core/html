9a2b3428a3777720c3602cacac2cff78ceaba73c
Brian Takita <brian.takita@gmail.com>
2021-11-16 21:22:55 -0600

    formatting

--- src/assign_style.ts
+++ src/assign_style.ts
@@ -1,6 +1,6 @@
 import { assign } from '@ctx-core/object'
-import { styles_o_ } from './styles_o_.js'
 import { style_ } from './style_.js'
+import { styles_o_ } from './styles_o_.js'
 /**
  * Assigns additional styles to the style attribute on the HTMLElement el.
  */
